89c3e8df4bd7b217455a12cc1dbc3b1dc344775b
Aditya8120 <aditya3.230101009@iiitbh.ac.in>
2026-01-21 01:18:50 +0530

    Employee Dashboard Responsiveness Update

--- src/components/Tasklist/TLists.jsx
+++ src/components/Tasklist/TLists.jsx
@@ -6,7 +6,7 @@ import FailedTask from './FailedTask'
 
 const TLists = ({data}) => {
     return (
-        <div id='TL' className='lg:h-1/2 w-full p-5 flex flex-col lg:flex-row items-stretch gap-5 lg:flex-nowrap overflow-auto'>
+        <div id='TL' className='lg:h-1/2 w-full p-5 flex flex-row items-stretch gap-5 flex-nowrap overflow-auto'>
             {data.tasks.map((e)=>{
                 if(e.newTask){
                     return <NewTask data={e}/>
